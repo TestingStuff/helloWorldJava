383784cf36dcc2a2596c6adf1b94621345bf265f
Thomas <contact@ryd.no>
2013-12-09 00:07:43 -0800

    java test

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -4,10 +4,18 @@ public class helloWorld {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-System.out.println("Hello world!");
+	
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+		
+	System.out.println("Hello world!\n New line");
+	System.out.println("Hello world!!");
+	int x = 10;
+	while( x < 12 ) {
+		System.out.print("value of x : " + x );
+		x++;
+		System.out.print("\n");
+		if (x == 11){ System.out.print ("Ten!\n");}
 	}
-
+}
 }
